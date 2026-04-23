43b941c8b5a17a11f3de357e9b877f53812cffb9
Kiineku <148930358+Kiineku@users.noreply.github.com>
2026-04-23 15:54:43 -0500

    Update pharmacy.sql
    Added DROP section
    Created basic tables for entities based on previous planning

---
+++ pharmacy.sql
@@ -0,0 +1,33 @@
+-- Drop
+DROP TABLE IF EXISTS sales_transaction;
+DROP TABLE IF EXISTS inventory;
+DROP TABLE IF EXISTS prescription;
+DROP TABLE IF EXISTS supplier;
+DROP TABLE IF EXISTS medication;
+DROP TABLE IF EXISTS doctor;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS patient;
+
+-- Patient
+CREATE TABLE patient();
+
+-- Employee
+CREATE TABLE employee();
+
+-- Doctor
+CREATE TABLE doctor();
+
+-- Medication
+CREATE TABLE medication();
+
+-- Prescription
+CREATE TABLE prescription();
+
+-- Inventory
+CREATE TABLE inventory();
+
+-- Supplier
+CREATE TABLE supplier();
+
+-- Sales Transaction
+CREATE TABLE sales_transaction();
