b9cebeec5a63ebc71e0c6e845c4e94a19024d6b9
Kiineku <148930358+Kiineku@users.noreply.github.com>
2026-04-23 16:22:01 -0500

    Update pharmacy.sql
    Added attributes for patient table

--- pharmacy.sql
+++ pharmacy.sql
@@ -9,7 +9,17 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS patient;
 
 -- Patient
-CREATE TABLE patient();
+CREATE TABLE patient(
+	ssn VARCHAR(11) PRIMARY KEY,
+	patient_id SERIAL,
+	fname VARCHAR(20),
+	lname VARCHAR(20),
+	dob DATE,
+	phone VARCHAR(20),
+	address TEXT,
+	medical_history TEXT,
+	insurance VARCHAR(50),
+);
 
 -- Employee
 CREATE TABLE employee();
